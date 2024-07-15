3488cfc6eee7c8e9eff078ede83af575e068fb26
Hailemchael Atrsaw <hailat.alx@gmail.com>
2024-07-15 16:13:10 +0300

    CSRF Trusted Origins changed

--- config/settings/dev_settings.py
+++ config/settings/dev_settings.py
@@ -18,7 +18,7 @@ DATABASES = {
     }
 }
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost:1337"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:2001"]
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
